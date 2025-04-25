4d321d207cc6ddccca035cdecfb80de77324a985
hubdubabadub <200143586+hubdubabadub@users.noreply.github.com>
2025-04-25 11:12:15 -0700

    Finalize submission, clean up

--- etch.js
+++ etch.js
@@ -1,11 +1,16 @@
 let container = document.querySelector(".container");
+let documentBody = document.querySelector("body");
 
-let resize = document.querySelector("#resize");
-resize.addEventListener("click", () => {
-  let size = prompt("Enter side length", '');
-  if ( size === '' ) {
-    generateGrid();
-  } else generateGrid(size)});
+function randomRGB() {
+  return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random()
+    * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+}
+
+function increaseOpacity(element) {
+  let op = Number(element.style.getPropertyValue("opacity"));
+  op += 0.1;
+  element.style.opacity = String(op);
+}
 
 function generateGrid(sideLength = 16) {
   let squared = (sideLength * sideLength);
@@ -29,12 +34,11 @@ function generateGrid(sideLength = 16) {
   }
 };
 
-function randomRGB() {
-  return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-}
-
-function increaseOpacity(element) {
-  let op = Number(element.style.getPropertyValue("opacity"));
-  op += 0.1;
-  element.style.opacity = String(op);
-}
+let resize = document.querySelector("#resize");
+resize.addEventListener("click", () => {
+  let size = prompt("Enter side length (max 100):\nDefault size: 16", '');
+  if ( size === '' ) {
+    generateGrid();
+  } else if ( size > 100 ) {
+    generateGrid(100);
+  } else generateGrid(size)});
